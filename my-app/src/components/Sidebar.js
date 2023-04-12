37aa4b0f96a75e4dc1ce2429c778716185adae8d
alcl2000 <amy.555.lewis@gmail.com>
2023-04-12 14:09:05 +0000

    add: side bar

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './css/Sidebar.module.css';
+
+const SideBar = () => {
+    return <div className={css.sidebar}>
+        <a href>My Photos</a>
+        <a href>My Illustrations</a>
+        <a href>My Paintings</a>
+    </div>
+};
+
+export default SideBar
