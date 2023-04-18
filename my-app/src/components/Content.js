e472225d4f1b56ad367ad63cbf70d05156d76524
alcl2000 <amy.555.lewis@gmail.com>
2023-04-18 16:35:40 +0000

    fix: class comp changed to functional with hooks

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,64 +1,92 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import css from './css/Content.module.css'
 import {savedPosts} from '../posts.json'
 import PostItem from './PostItem'
 import Loader from './Loader'
 
+// the class component
+// export default class Content extends Component{
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       'isLoaded' : false,
+//       'posts': []
+//     }
+//   }
 
-export default class Content extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      'isLoaded' : false,
-      'posts': []
-    }
-  }
+//   componentDidMount(){
+//     this.timerID = setTimeout(() => {
+//       this.setState({
+//         'isLoaded' : true,
+//         'posts': savedPosts
+//       })
+//     }, 1000
+//     );
+//     console.log('timer started')
+//   }
 
-  componentDidMount(){
-    this.timerID = setTimeout(() => {
-      this.setState({
-        'isLoaded' : true,
-        'posts': savedPosts
-      })
-    }, 1000
-    );
-    console.log('timer started')
-  }
+//   componentWillUnmount(){
+//     this.timerID = clearTimeout()
+//   }
 
-  componentWillUnmount(){
-    this.timerID = clearTimeout()
-  }
+//   handleChange(event){
+//     const name = event.target.value.toLowerCase()
+//     const filteredPosts = savedPosts.filter( post => {
+//         return post.name.toLowerCase().includes(name);
+//     })
+//     this.setState({
+//       'posts': filteredPosts
+//     })
+//   }
 
-  handleChange(event){
+//   render(){
+//       return (
+//           <div>
+              
+//           </div>
+//       );
+//      };
+// }
+
+// functional component with hooks
+
+export default function Content() {
+
+  const [isLoaded, loadPosts] = useState(false);
+  const [fetchedPosts, setFetchedPosts ] = useState([]);
+
+  useEffect(()=>{
+    setTimeout(() =>{
+      loadPosts(true),
+      setFetchedPosts(savedPosts)
+    },1000)
+  },[])
+
+  const handleChange = (event) => {
     const name = event.target.value.toLowerCase()
     const filteredPosts = savedPosts.filter( post => {
         return post.name.toLowerCase().includes(name);
     })
-    this.setState({
-      'posts': filteredPosts
-    })
+    setFetchedPosts(filteredPosts)
   }
 
-  render(){
-      return (
-          <div>
-              <div className={css.TitleBar}>
+  return (
+    <div>
+        <div className={css.TitleBar}>
                   My Photos
                   <form>
                     <label htmlFor="searchInput">Search:</label>
-                    <input type="search" placeholder="By Artist" id="searchInput" onChange={(e)=> this.handleChange(e)}/>
+                    <input type="search" placeholder="By Artist" id="searchInput" onChange={(e)=> handleChange(e)}/>
                   </form>
-                  <h4>post found:{this.state.posts.length}</h4>
+                  <h4>posts found: {fetchedPosts.length}</h4>
               </div>
               <div className={css.SearchResults}>
                   {
-                    this.state.isLoaded === false ?
+                    isLoaded === false ?
                       <Loader /> :
-                      < PostItem savedPosts={this.state.posts}/>
+                      < PostItem savedPosts={fetchedPosts}/>
                   }
               </div>
-          </div>
-      );
-     };
+    </div>
+  )
 }
-
